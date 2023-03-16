4111f7a0113192db49d084fb5a32dc84c4b508c8
nguyengiang080996 <nguyengiang08099676@gmail.com>
2023-03-16 19:47:16 +0700

    Add files via upload

---
+++ Openbrowser..cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XplorStudio
+{
+	public class Openbrowser
+	{
+        IWebDriver driver;
+
+        public Openbrowser()
+		{
+            driver = new ChromeDriver("/Applications/Google\\ Chrome.app");
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Url = "http://www.google.co.in";
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
+
